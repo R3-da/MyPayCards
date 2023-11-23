95cd6b1d4066186fc4089f25db07e7cee495fa34
R3-da <haddan.reda.pers@gmail.com>
2023-11-23 20:48:29 +0100

    run dev file changes

--- PaymentAPI/PaymentAPI/obj/Debug/net7.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net7.0/PaymentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4241d110ec96a167559144f8f0ce9800a6a5e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
